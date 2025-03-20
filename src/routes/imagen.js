a45d47342d1e7bc563990452ac5d967a85e17c9d
missael <alebermor999@gmail.com>
2025-03-20 01:10:15 -0600

    Corrección de directorio subir imagenes router

--- src/routes/imagen.js
+++ src/routes/imagen.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const multer = require('multer');
-const upload = multer({dest: '../uploads/'})
+const upload = multer({ dest: '../uploads/' })
 
 
 const router = express.Router();
 
-module.exports = (connection) => {
-  
 
-  router.post('/upload', upload.single('promocionFile'), (req, res)=> {
+router.post('/upload', upload.single('promocionFile'), (req, res) => {
     console.log(req.file);
     res.send('Termina');
-  });
+});
 
-  return router;
-};
+
+
+
+
+
