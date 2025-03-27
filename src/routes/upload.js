e667df36650b0c731d0fdf203033f6a290580a57
missael <alebermor999@gmail.com>
2025-03-27 14:59:43 -0600

    Corrección de insertar imagenes

--- src/routes/upload.js
+++ src/routes/upload.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const uploadController = require('../controllers/upload');
+const upload = require("../middleware/multer");
 
 const router = express.Router();
 
 module.exports = (connection) => {
-  const controller = uploadController(connection);
+  const controller = uploadController(connection); // ✅ Ahora sí es una función válida
 
-  router.post('/', controller);
- 
+  router.post('/', upload.single('image'), controller); // ✅ Añadimos `upload.single('image')`
 
   return router;
 };
