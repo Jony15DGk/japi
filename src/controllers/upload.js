148c8fbd70e74216f1be8fa06c7c37993385bf20
missael <alebermor999@gmail.com>
2025-03-27 18:48:02 -0600

    Correccion multer

--- src/controllers/upload.js
+++ src/controllers/upload.js
@@ -4,7 +4,14 @@ const upload = require("../middleware/multer");
 module.exports = (connection) => {
   return async (req, res) => {
     try {
-      const result = await cloudinary.uploader.upload(req.file.path);
+      const result = await cloudinary.uploader.upload_stream({ resource_type: "image" }, (error, result) => {
+        if (error) {
+          console.error("Error en Cloudinary:", error);
+          return res.status(500).json({ success: false, message: "Error en la subida" });
+        }
+        res.status(200).json({ success: true, message: "Uploaded!", data: result });
+      }).end(req.file.buffer);
+      
       res.status(200).json({
         success: true,
         message: "Uploaded!",
