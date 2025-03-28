847c15bc45250156885526450467d98bbe71e4de
missael <alebermor999@gmail.com>
2025-03-27 19:12:44 -0600

    Correccion cloudiary archivo

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -7,7 +7,12 @@ cloudinary.config({
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_SECRET_KEY
 });
-
+console.log("Cloudinary Config:", {
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET_KEY ? "OK" : "MISSING"
+  });
+  
 module.exports = cloudinary;
 
 
