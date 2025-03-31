825a5eea20d84f958c25474bee78153dc5c54f88
missael <alebermor999@gmail.com>
2025-03-31 14:05:08 -0600

    Insertar en tabla imagen

--- src/controllers/upload.js
+++ src/controllers/upload.js
@@ -3,6 +3,15 @@ const cloudinary = require("../utils/cloudinary");
 module.exports = (connection) => {
   return async (req, res) => {
     try {
+      const { promocion_id } = req.body; // Se espera recibir el ID de la promoción en el body
+
+      if (!promocion_id) {
+        return res.status(400).json({
+          success: false,
+          message: "Se requiere el ID de la promoción"
+        });
+      }
+
       if (!req.files || req.files.length === 0) {
         return res.status(400).json({
           success: false,
@@ -10,7 +19,6 @@ module.exports = (connection) => {
         });
       }
 
-      
       const allowedTypes = ['image/jpeg', 'image/png', 'image/gif', 'image/webp', 'image/svg+xml'];
       const invalidFiles = req.files.filter(file => !allowedTypes.includes(file.mimetype));
       
@@ -24,7 +32,7 @@ module.exports = (connection) => {
       const uploadPromises = req.files.map((file) => {
         return new Promise((resolve, reject) => {
           const uploadStream = cloudinary.uploader.upload_stream(
-            { resource_type: "auto" },
+            { resource_type: "image" },
             (error, result) => {
               if (error) {
                 reject(error);
@@ -39,11 +47,31 @@ module.exports = (connection) => {
 
       const results = await Promise.all(uploadPromises);
 
+      // Insertar las imágenes en la base de datos
+      const insertPromises = results.map((image) => {
+        return new Promise((resolve, reject) => {
+          connection.query(
+            "INSERT INTO imagen (url, public_id, promocion_idpromocion) VALUES (?, ?, ?)",
+            [image.secure_url, image.public_id, promocion_id],
+            (err, result) => {
+              if (err) {
+                reject(err);
+              } else {
+                resolve(result);
+              }
+            }
+          );
+        });
+      });
+
+      await Promise.all(insertPromises);
+
       res.status(200).json({
         success: true,
-        message: "Uploaded!",
+        message: "Imágenes subidas y guardadas correctamente",
         data: results
       });
+
     } catch (err) {
       console.error("Error inesperado:", err);
       res.status(500).json({
