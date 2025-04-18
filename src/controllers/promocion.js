9fa5eb0b0c692bc646e0e3590080569087b9c79b
missael <alebermor999@gmail.com>
2025-04-18 15:26:49 -0600

    Corrección de consulta por rango: longitud y latitud. STGeomFromText

--- src/controllers/promocion.js
+++ src/controllers/promocion.js
@@ -473,8 +473,8 @@ module.exports = (connection) => {
                         e.descripcion AS empresa_descripcion
                      FROM promocion AS p
                      INNER JOIN empresa AS e ON p.empresa_idempresa = e.idempresa
-                   WHERE ST_Distance_Sphere(e.ubicacion, ST_GeomFromText(CONCAT('POINT(', ?, ' ', ?, ')'))) <= ? AND p.eliminado = 0`,
-[parseFloat(lng), parseFloat(lat), rango]
+                  WHERE ST_Distance_Sphere(e.ubicacion, ST_PointFromText(CONCAT('POINT(', ?, ' ', ?, ')'))) <= ? AND p.eliminado = 0`, 
+[parseFloat(lat), parseFloat(lng), rango]
                 );
 
 
