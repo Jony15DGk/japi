8730e66be4c59819df52ca0b9b5fcb0e16586b58
missael <alebermor999@gmail.com>
2025-05-18 11:34:33 -0600

    Cambio en el archivo .env

--- src/utils/fire-base.js
+++ src/utils/fire-base.js
@@ -14,7 +14,7 @@ try {
       credential: admin.credential.cert({
         projectId: process.env.FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        // Manejo más robusto de la clave privada
+        // Manejo
         privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
       }),
     });
