e8c4b908aead3431e0053af97dd680ccea6d5258
missael <alebermor999@gmail.com>
2025-05-17 21:30:06 -0600

    Cambirar nombre de archivo json

--- src/config/generateFirebase.js
+++ src/config/generateFirebase.js
@@ -1,6 +1,6 @@
 const fs = require('fs'); 
 
-const json = JSON.parse(fs.readFileSync('src/config/notificacion-firebase.json', 'utf8'));
+const json = JSON.parse(fs.readFileSync('src/config/notificaciones-fbcm.json', 'utf8'));
 
 console.log(`FIREBASE_PROJECT_ID=${json.project_id}`);
 console.log(`FIREBASE_CLIENT_EMAIL=${json.client_email}`);
