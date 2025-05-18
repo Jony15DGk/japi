babe768de721652c3e7925ed058813fde2f21114
missael <alebermor999@gmail.com>
2025-05-18 07:38:23 -0600

    Verificar no repetivión de credenciales

--- src/utils/fire-base.js
+++ src/utils/fire-base.js
@@ -1,12 +1,17 @@
 const admin = require('firebase-admin');
 
-admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  }),
-});
+const admin = require('firebase-admin');
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    }),
+  });
+}
+
 
 console.log('EMAIL:', process.env.FIREBASE_CLIENT_EMAIL);
 console.log('PROJECT_ID:', process.env.FIREBASE_PROJECT_ID);
