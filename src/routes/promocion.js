4f8a85af49eb1acc49ca33c216e3e1a219c8832b
missael <alebermor999@gmail.com>
2025-05-19 14:52:42 -0600

    Cambiar consulta en promocion

--- src/routes/promocion.js
+++ src/routes/promocion.js
@@ -13,7 +13,7 @@ module.exports = (connection) => {
     router.patch('/promocion/:id', controller.actualizarPromocion);
     router.delete('/promocion/:id', controller.eliminarPromocion);
     router.post('/promocionGeneral', upload.array('images', 4), controller.promocionGeneral);
-    router.get('/promocionRango', controller.consultarPorRango);
+    router.post('/promocionRango', controller.consultarPorRango);
 
     return router;
 };
